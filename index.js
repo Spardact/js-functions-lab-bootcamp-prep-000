34f16657a277be598c30e3b1f0e84d142b7c9a24
Travis Clarke <spardact@gmail.com>
2019-08-14 19:28:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-var $name = 'Travis';
-var $holiday = 'Independence day';
-
-console.log('${name}:');
-console.log(Travis);
-
-function happyHolidays(string){
-  return("Happy holidays!")
-}
-
-function happyHolidaysTo(string){
-  return("Happy holidays, you!")
-}
-
-function happyHolidaysto(string){
- return("Happy ${holiday}, ${name}!")
+function happyHolidays() {
+  return "happy holidays!"
 }
