7822d9577cab4dd33a525069fa4f938475cccc5f
Travis Clarke <spardact@gmail.com>
2019-08-14 19:16:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(string){
   return("Happy holidays, you!")
 }
 
-function happyHolidaysto(Independence day, you){
+function happyHolidaysto('Independence day', 'you'){
   return("Happy Independence day, you!")
 }
